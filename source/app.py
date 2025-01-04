ed289bc9d545da9bf801567f1b7786715ee9161e
SolverShade <seconduacc07@gmail.com>
2025-01-03 20:19:25 -0500

    removed ui folder from source as it was unused. theme content is now stored in dist.

--- source/app.py
+++ source/app.py
@@ -3,6 +3,8 @@ from PySide6.QtWidgets import QApplication
 from env import ROOT_DIR
 from source.controller.shortcut_controller import ShortcutController
 from PySide6.QtCore import QFile, QTextStream
+
+# this import may appear unused, but allows the theme to import its resources
 from resources import breeze_pyside6
 
 def run():
